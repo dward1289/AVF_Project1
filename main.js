2235e46e36881c1067c17c1aee4bd009419b7f7c
devona <dward1289@fullsail.edu>
2012-11-21 23:10:23 -0500

    All HTML and JS completed.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //Wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
-
+	
+	var comingSoon = function (){
+		alert("Coming soon:\n-iOS and Android Native Features Information\n-JSON-P APIs\n-Research Week 2 and Week 3");
+	}
+	
+	comingSoon();
 });
