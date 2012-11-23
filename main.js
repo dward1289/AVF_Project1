847c32783f7f12163261dbaefed8e40931da219b
dward1289 <dward1289@fullsail.edu>
2012-11-22 20:35:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //Wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 	
+        var btnColor = document.getElementById("button");
+
 	var comingSoon = function (){
 		alert("Coming soon:\n-iOS and Android Native Features Information\n-JSON-P APIs\n-Research Week 2 and Week 3");
 	}
+
+        var btnChange = function () {
+                btnColor.style.background= "red";
+}
+ 
+        btnColor.onclick = btnChange();
+
 	
 	comingSoon();
 });
