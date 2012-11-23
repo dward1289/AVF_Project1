6a2a0c8930648405a6afe163eb094405d49b9cae
dward1289 <dward1289@fullsail.edu>
2012-11-22 20:38:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 	
-        var btnColor = document.getElementById("button");
+        var btnColor = document.getElementByTagName("button");
 
 	var comingSoon = function (){
 		alert("Coming soon:\n-iOS and Android Native Features Information\n-JSON-P APIs\n-Research Week 2 and Week 3");
